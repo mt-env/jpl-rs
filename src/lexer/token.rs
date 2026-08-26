3ee91616c86a278e0eda6738de25c4217da7da44
Henry Zheng <henry.zheng727@gmail.com>
2026-08-25 20:27:38 -0600

    add token and tokenkind

---
+++ src/lexer/token.rs
@@ -0,0 +1,64 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Token<'a> {
+    pub kind: TokenKind,
+    pub offset: usize,
+    pub str: &'a str,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum TokenKind {
+    // primitive values
+    True,
+    False,
+    FloatVal,
+    IntVal,
+    String,
+    Variable,
+
+    // keywords
+    Array,
+    Assert,
+    Else,
+    Fn,
+    If,
+    Image,
+    Let,
+    Print,
+    Read,
+    Show,
+    Return,
+    Struct,
+    Sum,
+    Then,
+    Time,
+    To,
+    Void,
+    Write,
+
+    // type keywords
+    BoolType,
+    FloatType,
+    IntType,
+
+    // punctuation
+    Colon,
+    Comma,
+    LCurly,
+    LParen,
+    LSquare,
+    RCurly,
+    RParen,
+    RSquare,
+
+    // operators
+    Dot,
+    Equals,
+    Op,
+
+    // misc
+    EndOfFile,
+    NewLine,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum LexError {}
