ce45d8c68ba8e7fc2f80359cad91bdf82a60feb1
Henry Zheng <henry.zheng727@gmail.com>
2026-09-01 09:36:17 -0600

    allow allocation through parser ctx

--- src/parser/parser_ctx.rs
+++ src/parser/parser_ctx.rs
@@ -2,14 +2,14 @@ use bumpalo::Bump;
 
 use crate::lexer::token::{Token, TokenKind};
 
-struct ParserCtx<'src, 'ast> {
-    alloc: &'ast mut Bump,
+pub(super) struct ParserCtx<'src, 'ast> {
+    alloc: &'ast Bump,
     tokens: Vec<Token<'src>>,
     curr_pos: usize,
 }
 
 impl<'src, 'ast> ParserCtx<'src, 'ast> {
-    fn new(alloc: &'ast mut Bump, tokens: Vec<Token<'src>>) -> ParserCtx<'src, 'ast> {
+    pub(super) fn new(alloc: &'ast mut Bump, tokens: Vec<Token<'src>>) -> ParserCtx<'src, 'ast> {
         ParserCtx {
             alloc,
             tokens,
@@ -17,11 +17,11 @@ impl<'src, 'ast> ParserCtx<'src, 'ast> {
         }
     }
 
-    fn peek(&self) -> Option<Token<'src>> {
+    pub(super) fn peek(&self) -> Option<Token<'src>> {
         self.tokens.get(self.curr_pos).copied()
     }
 
-    fn expect(&mut self, expected: TokenKind) -> Result<Token<'src>, ()> {
+    pub(super) fn expect(&mut self, expected: TokenKind) -> Result<Token<'src>, ()> {
         match self.peek() {
             Some(token) if token.kind == expected => {
                 self.curr_pos += 1;
@@ -31,12 +31,16 @@ impl<'src, 'ast> ParserCtx<'src, 'ast> {
         }
     }
 
-    fn expect_many(&mut self, expected: &[TokenKind]) -> Result<Token<'src>, ()> {
+    pub(super) fn expect_many(&mut self, expected: &[TokenKind]) -> Result<Token<'src>, ()> {
         for expected_kind in expected {
             if let Ok(token) = self.expect(*expected_kind) {
                 return Ok(token);
             }
         }
         todo!()
+    }
+
+    pub(super) fn alloc<A>(&self, value: A) -> &'ast A {
+        self.alloc.alloc(value)
     }
 }
