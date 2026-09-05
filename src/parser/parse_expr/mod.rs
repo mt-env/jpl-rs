5566f447d75277314b8ab47d5715392f0c6d07cd
Henry Zheng <henry.zheng727@gmail.com>
2026-09-05 11:43:56 -0600

    patch expr parser to use expect properly

--- src/parser/parse_expr/mod.rs
+++ src/parser/parse_expr/mod.rs
@@ -11,38 +11,31 @@ use crate::{
 pub(super) fn parse_expr<'src, 'ast>(
     ctx: &mut ParserCtx<'src, 'ast>,
 ) -> Result<&'ast ParsedExpr<'src, 'ast>, ParseError<'src>> {
-    let Some(Token { offset, kind, .. }) = ctx.peek() else {
-        todo!()
-    };
+    let Token { offset, kind, str } = ctx.expect_many(&[
+        TokenKind::True,
+        TokenKind::False,
+        TokenKind::FloatVal,
+        TokenKind::IntVal,
+        TokenKind::Variable,
+        TokenKind::LSquare,
+    ])?;
     let kind = match kind {
-        TokenKind::True => parse_bool(ctx)?,
-        TokenKind::False => parse_bool(ctx)?,
-        TokenKind::FloatVal => parse_float(ctx)?,
-        TokenKind::IntVal => parse_int(ctx)?,
-        TokenKind::Variable => parse_var(ctx)?,
+        TokenKind::True => ExprKind::Bool(true),
+        TokenKind::False => ExprKind::Bool(false),
+        TokenKind::FloatVal => parse_float(str, offset)?,
+        TokenKind::IntVal => parse_int(str, offset)?,
+        TokenKind::Variable => ExprKind::Var(str),
         TokenKind::LSquare => parse_array_literal(ctx)?,
-        _ => todo!(),
+        _ => unsafe { unreachable_unchecked() }, // safe because of expect_many
     };
 
     Ok(ParsedExpr::new(ctx, offset, kind))
 }
 
-fn parse_bool<'src, 'ast>(
-    ctx: &mut ParserCtx<'src, 'ast>,
-) -> Result<ExprKind<'src, 'ast, ()>, ParseError<'src>> {
-    let Token { kind, .. } = ctx.expect_many(&[TokenKind::True, TokenKind::False])?;
-    let value = match kind {
-        TokenKind::True => true,
-        TokenKind::False => false,
-        _ => unsafe { unreachable_unchecked() }, // safe because of expect_many
-    };
-    Ok(ExprKind::Bool(value))
-}
-
 fn parse_float<'src, 'ast>(
-    ctx: &mut ParserCtx<'src, 'ast>,
+    str: &'src str,
+    offset: usize,
 ) -> Result<ExprKind<'src, 'ast, ()>, ParseError<'src>> {
-    let Token { offset, str, .. } = ctx.expect(TokenKind::FloatVal)?;
     let Ok(parsed_float) = str.parse::<f64>() else {
         return Err(ParseError::new(
             offset,
@@ -53,9 +46,9 @@ fn parse_float<'src, 'ast>(
 }
 
 fn parse_int<'src, 'ast>(
-    ctx: &mut ParserCtx<'src, 'ast>,
+    str: &'src str,
+    offset: usize,
 ) -> Result<ExprKind<'src, 'ast, ()>, ParseError<'src>> {
-    let Token { offset, str, .. } = ctx.expect(TokenKind::IntVal)?;
     let Ok(parsed_int) = str.parse::<i64>() else {
         return Err(ParseError::new(
             offset,
@@ -65,17 +58,9 @@ fn parse_int<'src, 'ast>(
     Ok(ExprKind::Int(parsed_int))
 }
 
-fn parse_var<'src, 'ast>(
-    ctx: &mut ParserCtx<'src, 'ast>,
-) -> Result<ExprKind<'src, 'ast, ()>, ParseError<'src>> {
-    let Token { str, .. } = ctx.expect(TokenKind::Variable)?;
-    Ok(ExprKind::Var(str))
-}
-
 fn parse_array_literal<'src, 'ast>(
     ctx: &mut ParserCtx<'src, 'ast>,
 ) -> Result<ExprKind<'src, 'ast, ()>, ParseError<'src>> {
-    ctx.expect(TokenKind::LSquare)?;
     let mut elements = Vec::new();
     loop {
         if ctx.peek_is(TokenKind::RSquare) {
