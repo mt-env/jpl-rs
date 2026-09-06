69c3899ac70d39a02a2ba2677d6762c6a808d3a5
Henry Zheng <henry.zheng727@gmail.com>
2026-09-06 11:20:05 -0600

    bug fix: ignore empty lines in parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -20,9 +20,18 @@ pub fn parse<'src, 'ast>(
 ) -> Result<ParsedProgram<'src, 'ast>, ParseError<'src>> {
     let mut parser_ctx = ParserCtx::new(alloc, tokens);
     let mut parsed_program = Vec::new();
-    while let Some(token) = parser_ctx.peek()
-        && token.kind != TokenKind::EndOfFile
-    {
+    loop {
+        while parser_ctx.peek_is(TokenKind::NewLine) {
+            parser_ctx.expect(TokenKind::NewLine)?;
+        }
+
+        if parser_ctx
+            .peek()
+            .is_none_or(|token| token.kind == TokenKind::EndOfFile)
+        {
+            break;
+        }
+
         parsed_program.push(parse_cmd::parse_cmd(&mut parser_ctx)?);
     }
 
