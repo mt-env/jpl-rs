adaf3432e9032bb45dd074bab119c8e8a1daff3d
Henry Zheng <henry.zheng727@gmail.com>
2026-09-08 14:33:30 -0600

    add helpers for pratt parsing

--- src/parser/parser_ctx.rs
+++ src/parser/parser_ctx.rs
@@ -2,7 +2,7 @@ use bumpalo::Bump;
 
 use crate::{
     lexer::token::{Token, TokenKind},
-    parser::ast::{ParseError, ParseErrorKind},
+    parser::ast::{BinOp, ParseError, ParseErrorKind, UnOp},
 };
 
 pub(super) struct ParserCtx<'src, 'ast> {
@@ -22,6 +22,19 @@ impl<'src, 'ast> ParserCtx<'src, 'ast> {
 
     pub(super) fn peek(&self) -> Option<Token<'src>> {
         self.tokens.get(self.curr_pos).copied()
+    }
+
+    pub(super) fn peek_or_error(&self) -> Result<Token<'src>, ParseError<'src>> {
+        self.peek().ok_or_else(|| {
+            ParseError::new(
+                self.tokens.last().map_or(0, |t| t.offset),
+                ParseErrorKind::UnexpectedToken {
+                    expected: vec![],
+                    found: TokenKind::EndOfFile,
+                    value: "",
+                },
+            )
+        })
     }
 
     pub(super) fn expect(&mut self, expected: TokenKind) -> Result<Token<'src>, ParseError<'src>> {
@@ -51,6 +64,39 @@ impl<'src, 'ast> ParserCtx<'src, 'ast> {
 
     pub(super) fn peek_is(&self, expected: TokenKind) -> bool {
         matches!(self.peek(), Some(token) if token.kind == expected)
+    }
+
+    pub(super) fn try_peek_unop(&self) -> Option<UnOp> {
+        match self.peek() {
+            Some(token) if token.kind == TokenKind::Op => match token.str {
+                "-" => Some(UnOp::Neg),
+                "!" => Some(UnOp::Not),
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+
+    pub(super) fn try_peek_binop(&self) -> Option<BinOp> {
+        match self.peek() {
+            Some(token) if token.kind == TokenKind::Op => match token.str {
+                "+" => Some(BinOp::Add),
+                "-" => Some(BinOp::Sub),
+                "*" => Some(BinOp::Mul),
+                "/" => Some(BinOp::Div),
+                "%" => Some(BinOp::Mod),
+                "&&" => Some(BinOp::And),
+                "||" => Some(BinOp::Or),
+                "==" => Some(BinOp::Eq),
+                "!=" => Some(BinOp::NotEq),
+                "<" => Some(BinOp::Lt),
+                "<=" => Some(BinOp::Lte),
+                ">" => Some(BinOp::Gt),
+                ">=" => Some(BinOp::Gte),
+                _ => None,
+            },
+            _ => None,
+        }
     }
 
     pub(super) fn expect_many(
