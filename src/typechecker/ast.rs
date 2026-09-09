98a6801744a1eb63fc00b9318bc6f074ef92f499
Henry Zheng <henry.zheng727@gmail.com>
2026-09-09 12:30:14 -0600

    add typevalue and typeerror enums

--- src/typechecker/ast.rs
+++ src/typechecker/ast.rs
@@ -3,24 +3,38 @@ use crate::{
     parser::ast::{Binding, Cmd, Expr, LValue, Stmt, Type},
 };
 
-pub enum TypeValue {}
+pub enum TypeValue<'src, 'ast> {
+    Int,
+    Bool,
+    Float,
+    Array {
+        element_type: &'ast TypeValue<'src, 'ast>,
+        dimension: usize,
+    },
+    Struct {
+        name: &'src str,
+    },
+    Void,
+}
 
 pub type TypedProgram<'src, 'ast> = Vec<&'ast TypedCmd<'src, 'ast>>;
-pub type TypedCmd<'src, 'ast> = Spanned<Cmd<'src, 'ast, TypeValue>>;
-pub type TypedExpr<'src, 'ast> = Spanned<Expr<'src, 'ast, TypeValue>>;
+pub type TypedCmd<'src, 'ast> = Spanned<Cmd<'src, 'ast, TypeValue<'src, 'ast>>>;
+pub type TypedExpr<'src, 'ast> = Spanned<Expr<'src, 'ast, TypeValue<'src, 'ast>>>;
 pub type TypedLValue<'src> = Spanned<LValue<'src>>;
 pub type TypedType<'src, 'ast> = Spanned<Type<'src, 'ast>>;
-pub type TypedStmt<'src, 'ast> = Spanned<Stmt<'src, 'ast, TypeValue>>;
+pub type TypedStmt<'src, 'ast> = Spanned<Stmt<'src, 'ast, TypeValue<'src, 'ast>>>;
 pub type TypedBinding<'src, 'ast> = Spanned<Binding<'src, 'ast>>;
 
+pub type TypeError<'src, 'ast> = Spanned<TypeErrorKind<'src, 'ast>>;
+
 impl<'src, 'ast> TypedCmd<'src, 'ast> {
-    pub fn new(offset: usize, cmd: Cmd<'src, 'ast, TypeValue>) -> Self {
+    pub fn new(offset: usize, cmd: Cmd<'src, 'ast, TypeValue<'src, 'ast>>) -> Self {
         Spanned { offset, value: cmd }
     }
 }
 
 impl<'src, 'ast> TypedExpr<'src, 'ast> {
-    pub fn new(offset: usize, expr: Expr<'src, 'ast, TypeValue>) -> Self {
+    pub fn new(offset: usize, expr: Expr<'src, 'ast, TypeValue<'src, 'ast>>) -> Self {
         Spanned {
             offset,
             value: expr,
@@ -29,10 +43,17 @@ impl<'src, 'ast> TypedExpr<'src, 'ast> {
 }
 
 impl<'src, 'ast> TypedStmt<'src, 'ast> {
-    pub fn new(offset: usize, stmt: Stmt<'src, 'ast, TypeValue>) -> Self {
+    pub fn new(offset: usize, stmt: Stmt<'src, 'ast, TypeValue<'src, 'ast>>) -> Self {
         Spanned {
             offset,
             value: stmt,
         }
     }
+}
+
+pub enum TypeErrorKind<'src, 'ast> {
+    UnexpectedType {
+        expected: &'ast [TypeValue<'src, 'ast>],
+        found: &'ast TypeValue<'src, 'ast>,
+    },
 }
--- src/typechecker/mod.rs
+++ src/typechecker/mod.rs
@@ -1,7 +1,14 @@
-use crate::{parser::ast::ParsedCmd, typechecker::ast::TypedProgram};
+use crate::{
+    parser::ast::ParsedCmd,
+    typechecker::ast::{TypeError, TypedProgram},
+};
 
 pub mod ast;
+mod typecheck_cmd;
+mod typecheck_expr;
 
-pub fn typecheck<'src, 'old, 'new>(ast: Vec<ParsedCmd<'src, 'old>>) -> TypedProgram<'src, 'new> {
+pub fn typecheck<'src, 'old, 'new>(
+    ast: Vec<ParsedCmd<'src, 'old>>,
+) -> Result<TypedProgram<'src, 'new>, TypeError<'src, 'new>> {
     todo!()
 }
