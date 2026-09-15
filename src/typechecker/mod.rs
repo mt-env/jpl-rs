c0bd321df395ca3afe7268ee14ba56142b5383d8
Henry Zheng <henry.zheng727@gmail.com>
2026-09-15 12:35:54 -0600

    call main typecheck functions

--- src/typechecker/mod.rs
+++ src/typechecker/mod.rs
@@ -2,17 +2,26 @@ use bumpalo::Bump;
 
 use crate::{
     parser::ast::ParsedCmd,
-    typechecker::ast::{TypeError, TypedProgram},
+    typechecker::{
+        ast::{TypeError, TypedProgram},
+        typecheck_ctx::TypecheckCtx,
+    },
 };
 
 pub mod ast;
 mod typecheck_cmd;
 mod typecheck_ctx;
 mod typecheck_expr;
+mod typecheck_type;
 
 pub fn typecheck<'src, 'old, 'new>(
     alloc: &'new Bump,
     ast: Vec<&'old ParsedCmd<'src, 'old>>,
 ) -> Result<TypedProgram<'src, 'new>, TypeError<'src, 'new>> {
-    todo!()
+    let mut typed_ast = Vec::new();
+    let mut ctx = TypecheckCtx::new(alloc);
+    for cmd in ast {
+        typed_ast.push(typecheck_cmd::typecheck_cmd(&mut ctx, cmd)?)
+    }
+    Ok(typed_ast)
 }
