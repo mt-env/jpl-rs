632cb418b493ca7c71055afdc4391715457f4ae6
Henry Zheng <henry.zheng727@gmail.com>
2026-09-22 10:14:51 -0600

    rename copy-pasted parameter in typecheck_lvalue

--- src/typechecker/typecheck_lvalue/mod.rs
+++ src/typechecker/typecheck_lvalue/mod.rs
@@ -8,10 +8,10 @@ use crate::{
 
 pub(super) fn typecheck_lvalue<'src, 'old, 'new>(
     ctx: &mut TypecheckCtx<'src, 'new>,
-    binding: &'old ParsedLValue<'src>,
+    lvalue: &'old ParsedLValue<'src>,
 ) -> Result<&'new TypedLValue<'src>, TypeError<'src, 'new>> {
-    let loc = binding.offset;
-    match &binding.value {
+    let loc = lvalue.offset;
+    match &lvalue.value {
         LValue::Var(name) => Ok(TypedLValue::make_typed(ctx, loc, LValue::Var(name))),
         LValue::Array(arr, dimensions) => Ok(TypedLValue::make_typed(
             ctx,
